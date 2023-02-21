623d911fbaa0d8d49729cb007136bd463296a8df
Silvio Arem <silvio.arem@gmail.com>
2023-02-21 14:17:13 -0300

    feat: including logo and links in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
+import Link from "../Link";
+import Logo from "../Logo";
+import LogoHeader from "../../assets/icons/logo-header.jpg";
+
 export default function Header() {
     return (
         <header>
-            <h1>Header do site</h1>
+            <Logo alternative="Logo da loja" origin={LogoHeader} redirect="#"/>
+            <nav>
+                <Link texto="Sobre nós" redirect="#"/>
+            </nav>
         </header>
     );
 }
