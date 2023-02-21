cb6158ef7145f50baa1bb226fba241ce5388dc8d
Silvio Arem <silvio.arem@gmail.com>
2023-02-21 13:42:38 -0300

    feat: including logo attributes

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,13 @@
+interface LogoProps {
+    origin: string;
+    alternative: string;
+    redirect: string;
+}
 
-
-export default function Logo(props: ) {
+export default function Logo(props: LogoProps) {
     return (
-        <a href={}>
-            <img >
+        <a href={props.redirect}>
+            <img src={props.origin} alt={props.alternative}/>
         </a>
     )
 }
