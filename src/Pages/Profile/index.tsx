70f07357b2661fde1d5dc89d7d9388a78b790e85
AgstAngelo <agstangelo@gmail.com>
2023-02-22 18:15:06 -0300

    feat: Profile page functional, added react-router.

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -23,13 +23,12 @@ export default function Profile() {
 
   return (
       <section>
-        <>
         <h3>Dados Pessoais:</h3>
         {
           Object.keys(profileLabels)
           .map((label, index) => 
             <article key={index}>
-              <label htmlFor={label}>{profileLabels[label as keyof User]}</label>
+              <p>{profileLabels[label as keyof User]}</p>
               {
                 Object.keys(userProfile)
                 .filter(userkey => userkey === label)
@@ -37,35 +36,9 @@ export default function Profile() {
                   <p key={i}>{userProfile[user as keyof User]}</p>
                 )
               }
+              <button>Atualizar</button>
             </article>
         )}
-        </>
       </section>
   );
 }
-
-// Object.keys(Labels)
-//         .map((label, index) => 
-//           <article 
-//             className='
-//               h-12 w-full flex flex-row items-center justify-between 
-//               [&>*]:px-2odd:bg-slate-50 even:bg-rose-50'
-//             key={index}
-//           >
-//             <label key={index} className="w-full italic text-base sm:text-lg text-slate-600" htmlFor={label}>
-//               {Labels[label]}
-//             </label>
-//             {
-//               Object.keys(userProfile)
-//               .filter((key) => key === label)
-//               .map((userField, i) => 
-//                 <p key={i} className="w-full text-sm sm:text-base font-medium text-slate-600 ">
-//                   {userProfile[userField]}
-//                 </p>
-//               )
-//             }
-//             <button>
-//               <CiEdit className='h-6 w-6 sm:h-8 sm:w-8 text-slate-700'/>
-//             </button>
-//           </article> 
-//         )
