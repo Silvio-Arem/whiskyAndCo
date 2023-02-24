937ed5566ad380c405f4b1d4b27c6a60c45c9fe7
Silvio Arem <silvio.arem@gmail.com>
2023-02-24 12:04:22 -0300

    style: importing global style to the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Routes from "./Routes";
+import { GlobalStyle } from "./components/GlobalStyle";
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <GlobalStyle />
         <Header />
         <Routes />
         <Footer />
