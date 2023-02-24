99f9db91bf0506f63431222056828e23553dd47e
Silvio Arem <silvio.arem@gmail.com>
2023-02-24 10:42:03 -0300

    refactor: redirect BrowserRouter to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Admin from "./Pages/Admin";
-import Profile from "./Pages/Profile";
 import Routes from "./Routes";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,12 +5,12 @@ import Admin from "./Pages/Admin"
 
 export default function Routes() {
     return (
-        <BrowserRouter>
-            <WrapperRoutes>
-                <Route path="/" element={<Home />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/admin" element={<Admin />} />
-            </WrapperRoutes>
-        </BrowserRouter>
+
+        <WrapperRoutes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/admin" element={<Admin />} />
+        </WrapperRoutes>
+
     )
 }
