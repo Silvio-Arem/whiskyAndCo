7ee450cd8271d9526f58a3c86147de7e7327d73c
Silvio Arem <silvio.arem@gmail.com>
2023-02-24 12:03:12 -0300

    style: creating global style

---
+++ src/components/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        font-family: 'Oswald', sans-serif;
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+`;
