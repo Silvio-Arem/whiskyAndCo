97ed1ee07f90140b983413c175538240d881c1e6
Silvio Arem <silvio.arem@gmail.com>
2023-02-24 17:28:14 -0300

    refactor: deleting and updating files

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,18 @@
-import Links from "../Links";
 import Logo from "../Logo";
-import LogoHeader from "../../assets/icons/logo-header.jpg";
-import { HeaderDefault } from "./styles";
 
-
-export default function Header() {
+interface HeaderProps {
+    logoSrc: string;
+    logoAlt: string;
+    numItemsInCart: number;
+  }
+  
+export default function Header(props: HeaderProps) {
     return (
-        <HeaderDefault>
-            <Logo alternative="Logo da loja" origin={LogoHeader} redirect="/"/>
-            <nav>
-                <Links texto="Perfil" redirect="/profile"/>
-                <Links texto="Whiskys" redirect="/products"/>
-            </nav>
-
-        </HeaderDefault>
+      <header>
+        <Logo src={props.logoSrc} alt={props.logoAlt} />
+        <Nav />
+        <SearchBar />
+        <ShoppingCart numItems={props.numItemsInCart} />
+      </header>
     );
-}
+  }
