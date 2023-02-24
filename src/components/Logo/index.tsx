42a3e718f6329377b0f4138f924f137196428ee6
Silvio Arem <silvio.arem@gmail.com>
2023-02-24 17:23:00 -0300

    feat: updated logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,17 +2,16 @@ import { Link } from "react-router-dom";
 import { LogoDefault } from "./styles";
 
 interface LogoProps {
-    origin: string;
-    alternative: string;
-    redirect: string;
-}
-
+    src: string;
+    alt: string;
+  }
+  
 export default function Logo(props: LogoProps) {
     return (
-        <div>
-            <Link to={props.redirect}>
-                <LogoDefault src={props.origin} alt={props.alternative} />
-            </Link>
-        </div>
-    )
-}
+      <div className="logo">
+        <Link to="/">
+          <LogoDefault src={props.src} alt={props.alt} />
+        </Link>
+      </div>
+    );
+  }
