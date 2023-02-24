0415477d969985b141ffc05a682b64a9aa24ee9a
Silvio Arem <silvio.arem@gmail.com>
2023-02-24 19:02:28 -0300

    feat: created LoginIcon component

---
+++ src/components/LoginIcon/index.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function LoginIcon() {
+    return (
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+    );
+  }
