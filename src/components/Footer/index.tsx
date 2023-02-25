78a6283e225aaabde5ad4b02f40561de8e1ba9a4
Silvio Arem <silvio.arem@gmail.com>
2023-02-25 00:55:54 -0300

    style:  styling the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,35 @@
-export default function Footer() {
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { LinkItem } from '../Nav/styles';
+import { Link } from 'react-router-dom';
+import { ContainerFooter, LinkListFooter } from './styles';
+
+
+function Footer() {
     return (
-        <header>
-            <h1>Footer do site</h1>
-        </header>
+        <ContainerFooter>
+            <div>
+                <h4>Informações de Contato</h4>
+                <LinkListFooter>
+                    <li><FontAwesomeIcon icon={faMapMarkerAlt} /> Endereço: Rua Gama XP, 123 - Bairro - Cidade/Estado - CEP 12345-678</li>
+                    <li><FontAwesomeIcon icon={faPhone} /> Telefone: (11) 1234-5678</li>
+                    <li><FontAwesomeIcon icon={faEnvelope} /> Email: contato@email.com.br</li>
+                </LinkListFooter>
+            </div>
+            <div>
+                <LinkListFooter>
+                    <LinkItem><Link to="/products">Whiskys</Link></LinkItem>
+                    <LinkItem><Link to="/profile">Perfil</Link></LinkItem>
+                    <LinkItem><Link to="#">Sobre</Link></LinkItem>
+                    <LinkItem><Link to="/admin">Painel do Administrador</Link></LinkItem>
+                </LinkListFooter>
+            </div>
+            <div >
+                <p>Todos os direitos reservados &copy; 2023 Whisky&Co</p>
+            </div>
+        </ContainerFooter>
     );
 }
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ContainerFooter = styled.footer`
+    display: grid;
+    grid-auto-flow: row dense; 
+    grid-template-columns: 2fr 1fr; 
+
+`;
+
+export const LinkListFooter = styled.ul`
+    list-style: none;
+`;
+
