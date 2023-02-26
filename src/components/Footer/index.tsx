5dd1d7573287d1f6f4e9a542664603ac99af81e6
Silvio Arem <silvio.arem@gmail.com>
2023-02-26 12:45:48 -0300

    style:  styling the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,35 +1,104 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { LinkItem } from '../Nav/styles';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { ContainerFooter, LinkListFooter } from './styles';
+import { primaryColor, secondaryColor, tertiaryColor } from '../UI/variables';
 
+const StyledFooter = styled.footer`
+  background-color: ${secondaryColor};
+  padding: 30px;
 
-function Footer() {
-    return (
-        <ContainerFooter>
-            <div>
-                <h4>Informações de Contato</h4>
-                <LinkListFooter>
-                    <li><FontAwesomeIcon icon={faMapMarkerAlt} /> Endereço: Rua Gama XP, 123 - Bairro - Cidade/Estado - CEP 12345-678</li>
-                    <li><FontAwesomeIcon icon={faPhone} /> Telefone: (11) 1234-5678</li>
-                    <li><FontAwesomeIcon icon={faEnvelope} /> Email: contato@email.com.br</li>
-                </LinkListFooter>
-            </div>
-            <div>
-                <LinkListFooter>
-                    <LinkItem><Link to="/products">Whiskys</Link></LinkItem>
-                    <LinkItem><Link to="/profile">Perfil</Link></LinkItem>
-                    <LinkItem><Link to="#">Sobre</Link></LinkItem>
-                    <LinkItem><Link to="/admin">Painel do Administrador</Link></LinkItem>
-                </LinkListFooter>
-            </div>
-            <div >
-                <p>Todos os direitos reservados &copy; 2023 Whisky&Co</p>
-            </div>
-        </ContainerFooter>
-    );
+  .contact {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  h4 {
+    color: ${primaryColor};
+    font-size: 18px;
+    font-weight: 700;
+    margin-bottom: 20px;
+  }
+
+  ul {
+    color: ${tertiaryColor};
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  ul li {
+    margin-bottom: 10px;
+  }
+
+  ul li a {
+    color: ${tertiaryColor};
+    text-decoration: none;
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+      color: ${primaryColor};
+    }
+  }
+
+  ul li a:hover svg {
+    color: ${primaryColor};
+  }
+
+  .bottom-bar {
+    border-top: 1px solid color: ${tertiaryColor};;
+    margin-top: 30px;
+    padding-top: 20px;
+
+    p {
+      color: ${primaryColor};
+      text-align: center;  
+      font-size: 14px;
+      margin-bottom: 10px;
+    }
+
+    
+  }
+`;
+
+interface FooterProps {
+    className?: string;
 }
+
+const Footer: React.FC<FooterProps> = ({ className }) => {
+    return (
+        <StyledFooter className={className}>
+            <div className="container ">
+                <div className="row contact">
+                    <div className="col">
+                        <h4>Informações de Contato</h4>
+                        <ul>
+                            <li><FontAwesomeIcon icon={faMapMarkerAlt} /> Endereço: Rua Gama, 123 - Experience - Edição/46 - CEP 12345-678</li>
+                            <li><FontAwesomeIcon icon={faPhone} /> Telefone: (11) 1234-5678</li>
+                            <li><FontAwesomeIcon icon={faEnvelope} /> Email: contato@gama.com.br</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h4>Links Rápidos</h4>
+                        <ul>
+                            <li><Link to="/products">Whiskys</Link></li>
+                            <li><Link to="/profile">Perfil</Link></li>
+                            <li><Link to="#">Sobre</Link></li>
+                            <li><Link to="/admin">Painel do Administrador</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="container bottom-bar">
+                <div className="row">
+                    <div className="col">
+                        <p>&copy; 2023 Exemplo de E-commerce. Todos os direitos reservados.</p>
+                    </div>
+                </div>
+            </div>
+        </StyledFooter>
+    );
+};
 
 export default Footer;
