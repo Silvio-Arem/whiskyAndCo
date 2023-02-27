d6e94db823cdf068029c4b699a116349ba223664
Silvio Arem <silvio.arem@gmail.com>
2023-02-27 12:05:50 -0300

    feat: Changes in Nav

--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -1,10 +1,31 @@
 import styled from "styled-components";
+import { primaryColor, tertiaryColor } from "../UI/variables";
 
-export const LinkList = styled.ul`
-    display: flex;
-    list-style: none;
-`;
+export const NavContainer = styled.nav`
+ul {
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 
-export const LinkItem = styled.li`
-    padding: 5px;
+  li {
+    margin-left: 20px;
+
+    &:first-child {
+      margin-left: 0;
+    }
+
+    a {
+      color: ${primaryColor};
+      font-size: 16px;
+      text-decoration: none;
+      text-transform: uppercase;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${tertiaryColor};
+      }
+    }
+  }
+}
 `;
