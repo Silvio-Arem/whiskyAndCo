41c654d130c30c99456397cd8b55eb9ec14b6e92
Silvio Arem <silvio.arem@gmail.com>
2023-02-27 12:06:11 -0300

    feat: Changes in SearchBar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,16 @@
+import { SearchBarContainer } from "./styles";
+import Search from "../../assets/icons/search-icon.png"
+
+
 export default function SearchBar() {
-    return (
+  return (
+    <SearchBarContainer>
       <div className="busca">
         <form action="/busca" method="get">
-          <input type="text" name="termo_busca" placeholder="" />
-          <button type="submit">Buscar</button>
+          <input type="text" name="termo_busca" placeholder="Digite aqui sua busca" />
+          <button type="submit"><img src={Search} alt="Buscar" /></button>
         </form>
       </div>
-    );
-  }
+    </SearchBarContainer>
+  );
+}
---
+++ src/components/SearchBar/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { secondaryColor, tertiaryColor } from "../UI/variables";
+
+export const SearchBarContainer = styled.div`
+  .busca {
+    display: flex;
+
+    form {
+      display: flex;
+      margin-left: auto;
+      position: relative;
+
+      input {
+        background-color: ${secondaryColor};
+        border: none;
+        border-bottom: 1px solid #888;
+        font-size: 16px;
+        padding: 5px 10px;
+        width: 200px;
+
+        &:focus {
+          outline: none;
+          border-bottom-color: ${tertiaryColor};
+        }
+      }
+
+      button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        margin-left: 10px;
+        padding: 0;
+
+        img {
+          max-height: 24px;
+        }
+      }
+    }
+  }
+`;
