0d8fdbfdaa7d75b3563fc969f9109536f20a95b8
Silvio Arem <silvio.arem@gmail.com>
2023-02-27 12:49:07 -0300

    feat: Changes in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,69 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { primaryColor, secondaryColor, tertiaryColor } from '../UI/variables';
-
-const StyledFooter = styled.footer`
-  width: 100%;
-  background-color: ${secondaryColor};
-  padding: 30px;
-  margin: 15px auto auto auto;
-  bottom: 0px;
-
-  .contact {
-    display: flex;
-    justify-content: space-evenly;
-  }
-
-  h4 {
-    color: ${primaryColor};
-    font-size: 18px;
-    font-weight: 700;
-    margin-bottom: 20px;
-  }
-
-  ul {
-    color: ${tertiaryColor};
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  ul li {
-    margin-bottom: 10px;
-  }
-
-  ul li a {
-    color: ${tertiaryColor};
-    text-decoration: none;
-    transition: color 0.2s ease-in-out;
-
-    &:hover {
-      color: ${primaryColor};
-    }
-  }
-
-  ul li a:hover svg {
-    color: ${primaryColor};
-  }
-
-  .bottom-bar {
-    border-top: 1px solid color: ${tertiaryColor};;
-    margin-top: 30px;
-    padding-top: 20px;
-
-    p {
-      color: ${primaryColor};
-      text-align: center;  
-      font-size: 14px;
-      margin-bottom: 10px;
-    }
-
-    
-  }
-`;
+import { StyledFooter } from './styles';
 
 interface FooterProps {
   className?: string;
