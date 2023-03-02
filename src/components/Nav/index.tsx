c8b5aa923e4200db7e3fb5b53fb07758c3213ee4
Silvio Arem <silvio.arem@gmail.com>
2023-03-02 15:13:00 -0300

    feat: optimizing the Nav component

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,15 +1,12 @@
-import { Link } from "react-router-dom";
-import { NavContainer } from "./styles";
+import { MenuItem, NavContainer } from "./styles";
 
 export default function Nav() {
     return (
       <NavContainer>
-      <ul>
-        <li><Link to="/products" title="Ir para whiskys">Whiskys</Link></li>
-        <li><Link to="/profile" title="Ir para  perfil">Perfil</Link></li>
-        <li><Link to="#" title="Ir para informações sobre a loja">Sobre</Link></li>
-        <li><Link to="/admin" title="Ir para painel administrativo">Painel do Administrador</Link></li>
-      </ul>
+        <MenuItem to="/products" title="Ir para whiskys">Whiskys</MenuItem>
+        <MenuItem to="/profile" title="Ir para  perfil">Perfil</MenuItem>
+        <MenuItem to="#" title="Ir para informações sobre a loja">Sobre</MenuItem>
+        <MenuItem to="/admin" title="Ir para painel administrativo">Painel do Administrador</MenuItem>
     </NavContainer>
     );
   }
