751b018e1630251df954fd1e06d3caf9d268a06a
Silvio Arem <silvio.arem@gmail.com>
2023-03-02 15:13:45 -0300

    style: adjusting Nav responsiveness

--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -1,31 +1,101 @@
+import { secondaryColor } from './../UI/variables';
+import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import { primaryColor, tertiaryColor } from "../UI/variables";
+import MenuIcon from "../../assets/icons/menu-icon.png";
 
 export const NavContainer = styled.nav`
-ul {
   display: flex;
   list-style: none;
   margin: 0;
   padding: 0;
+  align-items: center;
+  gap: 15px;
 
-  li {
-    margin-left: 20px;
+@media (max-width: 1023px) {
+    display: none;
+  }
+`;
 
-    &:first-child {
-      margin-left: 0;
-    }
+export const MenuItem = styled(Link)`
+    color: ${primaryColor};
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    transition: color 0.2s;
 
-    a {
-      color: ${primaryColor};
-      font-size: 16px;
-      text-decoration: none;
-      text-transform: uppercase;
-      transition: color 0.2s;
-
-      &:hover {
-        color: ${tertiaryColor};
+    &:hover {
+      color: ${tertiaryColor};
       }
+`;
+
+export const MenuButton = styled.button`
+  background-color: ${primaryColor};
+  border: 2px solid ${tertiaryColor};
+  border-radius: 20%;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 24px;
+  width: 24px;
+  padding: 0;
+  background-image: url(${MenuIcon});
+  background-size: cover;
+  background-position: center;
+  display: none;
+
+  &:focus {
+    outline: none;
+  }
+
+  span {
+    display: block;
+    height: 2px;
+    background-color: ${tertiaryColor};
+    transition: transform 0.2s;
+    transform-origin: center;
+  }
+
+  &.active span:first-of-type {
+    transform: translateY(8px) rotate(45deg);
+  }
+
+  &.active span:last-of-type {
+    transform: translateY(-8px) rotate(-45deg);
+  }
+
+  &.active span:nth-of-type(2) {
+    opacity: 0;
+  }
+
+  @media (max-width: 1023px) {
+    display: flex;
+  }
+
+`;
+
+interface MenuProps {
+  open?: boolean;
+}
+
+export const Menu = styled.div<MenuProps>`
+  display: none;
+
+  @media (max-width: 1023px) {
+    display: ${({ open }) => (open ? "block" : "none")};
+    position: absolute;
+    top: 80px;
+    left: 0;
+    right: 0;
+    padding: 20px;
+    background-color: ${secondaryColor};
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+    z-index: 1;
+
+    ${MenuItem} {
+      display: block;
+      margin-bottom: 10px;
     }
   }
-}
 `;
