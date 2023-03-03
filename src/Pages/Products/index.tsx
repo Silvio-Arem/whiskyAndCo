9ea4a54d7ff1aaa4f84cc258c8e7e9b077f53023
Silvio Arem <silvio.arem@gmail.com>
2023-03-03 13:12:32 -0300

    DESCRIÇÃO: WOODFORD RESERVE É UM LUXUOSO BOURBON, CUIDADOSAMENTE PRODUZIDO À MÃO EM PEQUENOS LOTES NA WOODFORD RESERVE DISTILLERY. TRADICIONAL DESDE 1812, É A DESTILARIA MAIS ANTIGA DOS ESTADOS UNIDOS. TRIPLAMENTE DESTILADA E ENVELHECIDA SOB MEDIDA AO LONGO DO TEMPO EM BARRIS DE CARVALHO BRANCO AMERICANO. É O UNICO BOURBON DO MUNDO A TER BARRIS CRIADOS ESPECIFICAMENTE PARA SUA PRODUÇÃO. O MASHBILL PARA WOODFORD RESERVE APRESENTA UMA ELEVADA PORCENTAGEM DE CENTEIO: 72% DO MILHO, 18% DE CENTEIO E 10% DE MALTE.

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,9 +1,25 @@
-function Products() {
-    return (
-      <div className="Products">
-        <h1>Página produtos</h1>
+import { IProduct } from "../../interfaces";
+import { products } from "../../data";
+
+export default function Products() {
+
+  const product = products.map((item: IProduct) => (
+    <div key={item.id} className="products__cards">
+      <img src={item.img} alt={item.name} />
+      <div className="products__cards-body">
+        <h5>{item.name}</h5>
+        <p>Preço: {item.price}</p>
+        <p>Descrição: {item.description}</p>
       </div>
-    );
-  }
-  
-  export default Products;
+    </div>
+  ))
+
+
+  return (
+    <>
+      <section className="Products">
+        {product} 
+      </section>
+    </>
+  );
+}
