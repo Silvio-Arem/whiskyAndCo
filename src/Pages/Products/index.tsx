ec54a65d76acf5be3e724ec5ca8d8631639df724
Silvio Arem <silvio.arem@gmail.com>
2023-03-04 19:31:02 -0300

    feat: Products page created

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,25 +1,52 @@
 import { IProduct } from "../../interfaces";
 import { products } from "../../data";
+import { Link } from "react-router-dom";
+import { ProductsContainer } from "./styles";
+import { Filter } from "../../components/Filter/styles";
+
 
 export default function Products() {
+
 
   const product = products.map((item: IProduct) => (
     <div key={item.id} className="products__cards">
       <img src={item.img} alt={item.name} />
       <div className="products__cards-body">
         <h5>{item.name}</h5>
-        <p>Preço: {item.price}</p>
-        <p>Descrição: {item.description}</p>
+        <p>R$ {item.price}</p>
+        <p>{item.category}</p>
+        <div className="products__card-button">
+          <Link to="/product" title="Ir para whisky selecionado">Descrição</Link>
+        </div>
       </div>
     </div>
   ))
 
-
   return (
     <>
-      <section className="Products">
-        {product} 
-      </section>
+      <Filter>
+        <details>
+          <summary>
+            Categoria
+          </summary>
+          <label htmlFor=""><input type="checkbox" />Whisky Japonês</label>
+          <label htmlFor=""><input type="checkbox" />Tennesse Whiskey</label>
+          <label htmlFor=""><input type="checkbox" />Bourbon Whiskey</label>
+          <label htmlFor=""><input type="checkbox" />Rye</label>
+        </details>
+        <details>
+          <summary>
+            Marca
+          </summary>
+          <label htmlFor=""><input type="checkbox" />Johnnie Walker</label>
+          <label htmlFor=""><input type="checkbox" />Jack Daniels</label>
+          <label htmlFor=""><input type="checkbox" />Ballantines</label>
+          <label htmlFor=""><input type="checkbox" />Chivas Regal</label>
+        </details>
+      </Filter>
+      <ProductsContainer className="Products">
+        {product}
+      </ProductsContainer>
     </>
   );
 }
---
+++ src/Pages/Products/styles.ts
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import { primaryColor, secondaryColor, tertiaryColor } from '../../components/UI/variables';
+
+
+export const ProductsContainer = styled.section`
+  margin: 14px 20%;
+
+  .products__cards {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  border: 2px solid ${primaryColor};
+  border-radius: 5px;
+  margin: 10px;
+  padding: 10px;
+}
+
+.products__cards img {
+  border: 4px solid ${primaryColor};
+  width: 100%;
+  max-width: 300px;
+}
+
+.products__cards-body {
+  text-align: center;
+  padding: 10px;
+}
+
+.products__cards-body h5 {
+  font-size: 21px;
+  margin: 10px 0;
+}
+
+.products__cards-body p {
+  font-size: 16px;
+  margin: 5px 0;
+}
+
+.products__card-button {
+  margin: 10px 0;
+}
+
+
+.products__card-button a {
+  display: block;
+  padding: 10px;
+  background-color: ${secondaryColor};
+  border: 4px solid ${primaryColor};
+  border-radius: 5px;
+  text-align: center;
+  color: ${primaryColor};
+  text-decoration: none;
+  font-size: 1rem;
+
+  &:hover {
+  background-color: ${tertiaryColor};
+}
+}
+
+@media (min-width: 768px) {
+  .products__cards {
+    flex-direction: row;
+    max-width: 700px;
+  }
+
+  .products__cards img {
+    max-width: 300px;
+  }
+
+  .products__cards-body {
+    padding: 10px 20px;
+    text-align: left;
+  }
+
+  .products__cards-body h5 {
+    font-size: 1.5rem;
+  }
+
+  .products__cards-body p {
+    font-size: 1.2rem;
+  }
+
+  .products__card-button {
+    margin: 0;
+    text-align: center;
+  }
+}
+
+`;
+
