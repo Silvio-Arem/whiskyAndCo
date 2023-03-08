9be0cff5782cb457b0ef392aff9eaa4e3d2b4501
Rodrigo Oliveira <rodrigoyenow@hotmail.com>
2023-03-08 15:09:01 -0300

    feat/Register

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import {FormRegister, InputDiv} from "./styles"
 
 const RegisterPage = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [cpf, setCpf] = useState<string>("");
+    const [address, setAddress] = useState<string>("");
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log("submit", {name, cpf, email, password, address});
+    }
+
+
     return(
         <div>
-        <FormRegister>
+        <FormRegister onSubmit={handleSubmit}>
         <h1>Faça seu cadastro</h1>
         
             <InputDiv>
             <label htmlFor="name"></label>
-            <input type="name" name="name" placeholder="Nome completo" />
+            <input type="name" name="name" placeholder="Nome completo" value={name} onChange= {(event) => setName(event.target.value)}/>
             </InputDiv>
 
             <InputDiv>
             <label htmlFor="cpf"></label>
-            <input type="cpf" name="cpf" placeholder="CPF" />
+            <input type="cpf" name="cpf"  placeholder="CPF" value={cpf} onChange= {(event) => setCpf(event.target.value)}/>
             </InputDiv>
 
             <InputDiv>
             <label htmlFor="email"></label>
-            <input type="email" name="email" placeholder="Email" />
+            <input type="email" name="email" placeholder="Email" value={email} onChange= {(event) => setEmail(event.target.value)}/>
             </InputDiv>
 
             <InputDiv>
             <label htmlFor="password"></label>
-            <input type="password" name="password" placeholder="Senha" />
+            <input type="password" name="password" placeholder="Senha" value={password} onChange= {(event) => setPassword(event.target.value)}/>
             </InputDiv>
 
              <InputDiv>
             <label htmlFor="address"></label>
-            <input type="address" name="address" placeholder="Endereço" />
+            <input type="address" name="address" placeholder="Endereço" value={address} onChange= {(event) => setAddress(event.target.value)}/>
             </InputDiv>
 
             <div>
