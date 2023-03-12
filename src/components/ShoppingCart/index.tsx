ffaeffefde3e7b464eee5f5595a32b9acdc430f9
Silvio Arem <silvio.arem@gmail.com>
2023-03-12 13:04:21 -0300

    feat: adjusted product value and returning to the attempt to reinstate the cart

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import CartIcon from "../../assets/icons/cart-icon.png";
 import { ShoppingCartContainer } from "./styles";
 import Button from '../Button';
-import { useLocation } from 'react-router-dom';
 import { IProduct } from '../../interfaces';
 
 interface ShoppingCartProps {
@@ -12,27 +11,10 @@ interface ShoppingCartProps {
 }
 
 export default function ShoppingCart(props: ShoppingCartProps) {
-    const { state } = useLocation();
-  const [ product, setProduct ] = useState <IProduct> ({
-    _id: "",
-    name: "",
-    category: {_id: "", name: ""},
-    brand: {_id: "", name: ""},
-    picture: "",
-    price: 0,
-    description: "",
-  });
 
-  useEffect(() => {
-    if(state) {
-      setProduct(state);
-    }
-    else {
-
-    }
-  }, [])
-
+    
     const [showModal, setShowModal] = useState(false);
+
 
     const handleShow = () => {
         setShowModal(true);
@@ -51,18 +33,11 @@ export default function ShoppingCart(props: ShoppingCartProps) {
                     <Modal.Title>Carrinho</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                {props.cartItems.map((product) => (
-                        <div key={product._id}>
-                            <h4>{product.name}</h4>
-                            <p>{product.description}</p>
-                            <p>Preço: R$ {product.price.toFixed(2)}</p>
-                        </div>
-                    ))}
                 </Modal.Body>
                 <Modal.Footer>
                     <div>
-                    <Button link='' title={''} text={'Continuar comprando'} onClick={() => setShowModal(false)}/>
-                    <Button title={''} text={'Finalizar sua compra!'} link={'checkout'} />
+                        <Button link='' title={''} text={'Continuar comprando'} onClick={() => setShowModal(false)} />
+                        <Button title={''} text={'Finalizar sua compra!!'} link={'checkout'} />
                     </div>
                 </Modal.Footer>
             </Modal>
