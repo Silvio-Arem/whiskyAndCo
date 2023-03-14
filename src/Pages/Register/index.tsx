52fd2753bf076f0b116f4c04c007a93a2c1582be
AgstAngelo <agstangelo@gmail.com>
2023-03-13 22:35:09 -0300

    Register page update.

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -1,76 +1,90 @@
-import React, { useState } from "react";
+import { AxiosResponse } from "axios";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+import { IUser } from "../../interfaces";
+import { instance } from "../../requestConfig";
 import { RegisterUser } from "./registerApi";
 import {FormRegister, InputDiv} from "./styles"
 
 const RegisterPage = () => {
 
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [name, setName] = useState<string>("");
-    const [cpf, setCpf] = useState<string>("");
-    const [address, setAddress] = useState<string>("");
-    const [isAdmin, setisAdmin] = useState<boolean>(false)
+    const{ loggedUser, userToken } = useContext(AuthContext);
+
+    const [ user, setUser ] = useState <IUser> ({
+        name: "",
+        email: "",
+        cpf: "",
+        password: "",
+        address: "",
+        isAdmin: false,
+        userOrders: []
+      });
 
     const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        console.log("submit", {name, cpf, email, password, address, isAdmin});
+        try {
+            event.preventDefault();
+            let response: AxiosResponse<any, any>
+            const config = {
+                headers: {
+                  Authorization: `Bearer ${userToken}`
+            }};
+            const data = { ...user };
+
+            if(user.isAdmin) {
+                loggedUser.isAdmin
+                ? response = await instance.post("/user/admin", data, config)
+                : alert("Você não tem permissão para cadastar usuário administrador.");
+            } 
+            else {
+                response = await instance.post("/user", data);
+            } 
+        } catch (error) {
+            console.log("ERRO: ", error);
+        }
     }
-const payload ={
-    name,
-    email,
-    password,
-    cpf,
-    address,
-    isAdmin
     
-    
-};
-try{
-    const response = RegisterUser(payload)
-    
-} catch (error) {}
+    const handleValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        if(user !== null){
+          setUser({...user, [name]: value})
+        }
+      }
 
     return(
-        <div>
         <FormRegister onSubmit={handleSubmit}>
         <h1>Faça seu cadastro</h1>
         
             <InputDiv>
             <label htmlFor="name"></label>
-            <input type="name" name="name" required placeholder="Nome completo" value={name} onChange= {(event) => setName(event.target.value)}/>
+            <input type="text" name="name" required placeholder="Nome completo" value={user.name} onChange= {(e) => handleValues(e)}/>
             </InputDiv>
 
             <InputDiv>
             <label htmlFor="cpf"></label>
-            <input type="cpf" name="cpf"  required placeholder="CPF" value={cpf} onChange= {(event) => setCpf(event.target.value)}/>
+            <input type="cpf" name="cpf"  required placeholder="CPF" value={user.cpf} onChange= {(e) => handleValues(e)}/>
             </InputDiv>
 
             <InputDiv>
             <label htmlFor="email"></label>
-            <input type="email" name="email" required placeholder="Email" value={email} onChange= {(event) => setEmail(event.target.value)}/>
+            <input type="email" name="email" required placeholder="Email" value={user.email} onChange= {(e) => handleValues(e)}/>
             </InputDiv>
 
             <InputDiv>
             <label htmlFor="password"></label>
-            <input type="password" name="password" required placeholder="Senha" value={password} onChange= {(event) => setPassword(event.target.value)}/>
+            <input type="password" name="password" required placeholder="Senha" value={user.password} onChange= {(e) => handleValues(e)}/>
             </InputDiv>
 
              <InputDiv>
             <label htmlFor="address"></label>
-            <input type="address" name="address" required placeholder="Endereço" value={address} onChange= {(event) => setAddress(event.target.value)}/>
+            <input type="address" name="address" required placeholder="Endereço" value={user.address} onChange= {(e) => handleValues(e)}/>
             </InputDiv>
             <label htmlFor="">Sou um administrador</label>
-            <input type="checkbox" name="isAdmin" onChange={(event)=> setisAdmin(event.target.checked)}/>
+            <input type="checkbox" name="isAdmin" checked={user.isAdmin} onChange= {(e) => handleValues(e)}/>
             <div>
-                <button>Cadastrar</button>
+                <button type="submit">Cadastrar</button>
             </div>
 
-            
             </FormRegister>
-
-        
-       
-        </div>
     )
 }
 export default RegisterPage;
