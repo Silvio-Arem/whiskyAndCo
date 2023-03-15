6c0e5274ddc65eef12af7156549357a7c99e8212
AgstAngelo <agstangelo@gmail.com>
2023-03-14 21:23:25 -0300

    order page.

--- src/Pages/Order/index.tsx
+++ src/Pages/Order/index.tsx
@@ -1,21 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthContext';
 import { IOrder, IProduct } from "../../interfaces";
 import { instance } from '../../requestConfig';
+import { StyledOrder } from "./styles";
 
 export default function Order () {
 
   const { state } = useLocation();
-  const currentOrder: IOrder = state;
-  let list: IProduct[];
+  const { userToken } = useContext(AuthContext);
+
+  const currentOrder: IOrder = state.item;
+
+  const [ list, setList ] = useState<IProduct[]>([])
+  const [ order, setOrder ] = useState<IOrder>({
+    userId: { _id: "", name: ""},
+    products: [],
+    amount: 0,
+  })
+
+  const config = {headers: { Authorization: `Bearer ${userToken}`}};
 
   const getProducts = async () => {
     const response = await instance.get("/product");
-    const productsList = response.data;
-    const list = currentOrder.products
-      .forEach(element => {
-        productsList.filter((item: IProduct) => element.productId === item._id)
-    });
+    const products: IProduct[] = response.data;
+    setList(products);
+  }
+
+  const cancelOrder = async () => {
+    try {
+      const response = await instance.delete(`/order/${currentOrder._id}`, config);
+      alert("Pedido Cancelado com sucesso!");
+    } catch (error) {
+      console.log("ERRO: ", error);
+    }
+
   }
 
  useEffect(() => {
@@ -24,26 +43,28 @@ export default function Order () {
   }
  }, []);
 
-
-
   return (
-    <section>
+    <StyledOrder>
       <h3>Id do Pedido: {currentOrder._id}</h3>
       <p>Data do Pedido: {currentOrder.createdAt}</p>
       <article>
-        {
-          currentOrder.products
-          .map((item, index) => 
-            <div key={index}>
-              <p>{list[index].picture}</p>
-              <p>{list[index].name}</p>
-              <p>{currentOrder.products[index].quantity}</p>
-              <p>Valor: {list[index].price * currentOrder.products[index].quantity}</p>
-            </div>
-          )
-        }
+        {currentOrder.products.map((orderProduct) => {
+            const product = list.find((product) => product._id === orderProduct._id);
+            if (!product) {
+              return <p key={orderProduct._id}>Produto não encontrado</p>;
+            }
+            return (
+              <div key={orderProduct._id}>
+                <p>{product.picture}</p>
+                  <p>{product.name}</p>
+                  <p>{orderProduct.quantity}</p>
+                  <p>Valor: {product.price * orderProduct.quantity}</p>
+              </div>
+            );
+          })}
       </article>
-    <p>Total da compra: {currentOrder.amount}</p>
-    </section>
+      <p>Total da compra: {currentOrder.amount}</p>
+      <button onClick={() => cancelOrder()}>Cancelar Pedido</button>
+    </StyledOrder>
   )
 }
---
+++ src/Pages/Order/styles.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import { primaryColor, secondaryColor, tertiaryColor } from '../../components/UI/variables';
+
+export const StyledOrder = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 2rem;
+  
+  h3 {
+    font-size: 2rem;
+    font-weight: 500;
+  }
+
+  button, select, input {
+    outline: transparent;
+    border: .125rem solid ${primaryColor};
+    &:hover, &:focus {
+      border: .25rem solid ${primaryColor};
+      box-shadow: rgba(0, 0, 0, 0.16) 0px .25rem 1rem, rgba(0, 0, 0, 0.23) 0px .25rem 1rem;
+    }
+  }
+
+  button, select, input[type="submit"] {
+    text-transform: uppercase;
+    margin: .15rem;
+    padding: .25rem;
+    background-color: ${secondaryColor};
+    color: ${tertiaryColor};
+    &:hover, &:focus {
+      color: ${primaryColor};
+    }
+  }
+
+  select, input {
+    height: 2.5rem;
+  }
+  
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  div, ul {
+    width: 100%;
+    max-width: 48rem;
+    padding: 2rem;
+  }
+
+  select {
+    width: 33%;
+  }
+
+  button {
+    min-width: 6rem;
+  }
+
+  input {
+    text-transform: none;
+    width: 66%;
+    padding: .5rem;
+    border: 2px solid ${secondaryColor};
+    &:focus {
+      border: 3px solid ${primaryColor};
+      box-shadow: rgba(0, 0, 0, 0.16) 0px .25rem 1rem, rgba(0, 0, 0, 0.23) 0px .25rem 1rem;
+    }
+  }
+  a{
+    text-decoration: none;
+    color: ${secondaryColor};
+    &:hover, &:focus {
+      color: ${primaryColor};
+      font-size: large;
+    }
+  }
+
+  li {
+    list-style-type: none;
+    &:hover {
+      background-color: ${secondaryColor};
+    }
+  }
+
+  label, p:nth-child(even) {
+    font-weight: 500;
+    font-size: 1.25rem;
+  }
+`;
